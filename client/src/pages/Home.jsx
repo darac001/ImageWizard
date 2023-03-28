2a3d8276f7b5de0a337cafc27f4072b53814833c
darac001 <drxmrx861@gmail.com>
2023-03-28 14:04:41 -0600

    edit v3

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,10 +25,10 @@ const Home = () => {
 
   // console.log(allPosts);
   const fetchPosts = async () => {
-    setLoading(true);
 
     try {
-      const response = await fetch('https://imagewizard.onrender.com/api/v1/post', {
+      // const response = await fetch('https://imagewizard.onrender.com/api/v1/post', {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
